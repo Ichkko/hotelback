670698cc23dd53a7859a733efcd42c6b5042ea4c
Ichkko <ichkoog79@gmail.com>
2026-03-22 18:29:08 +0700

    error

---
+++ src/main/java/org/springframework/boot/autoconfigure/flyway/HotelDto.java
@@ -0,0 +1,7 @@
+package org.springframework.boot.autoconfigure.flyway;
+
+/**
+ * DTO for {@link com.example.hotelback.model.Hotel}
+ */
+public class HotelDto {
+  }
---
+++ src/main/java/org/springframework/boot/autoconfigure/flyway/OracleConfigurationExtension.java
@@ -0,0 +1,7 @@
+package org.springframework.boot.autoconfigure.flyway;
+
+/**
+ * DTO for {@link com.example.hotelback.model.Amenity}
+ */
+public class OracleConfigurationExtension {
+  }
